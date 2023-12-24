285bb55353ea151294f6c8d90d5f97363fd31a8d
Tanmay160301 <bhurketanmay@gmail.com>
2023-12-24 09:49:13 +0530

    app.jsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+/**
+ * 
+ *  Basic expectation from this code:
+ *  Basic understanding of what are hooks
+ *  basic understanding about what is useState 
+ * 
+ *  basic understanding about its syntax : 
+ *  let [counter, setCounter] = useState(default Value)
+ * 
+ *  Practically implemented useState hook by creating a counter Project
+ *  one counter is there , 2 buttons are there, One is increment and the other one is decrement 
+ *  increment - adds counter by one
+ *  decrement - substracts the counter by one
+ * 
+ * 
+ */
 
+
+function App() {
+  let [counter, setCounter] = useState(0);
+
+
+  function increment(){
+    // if (counter>=20) {
+    //   setCounter(20)
+    // }
+    // else
+    // {
+    //   setCounter(counter + 1);
+    // }
+
+    (counter>=20)?setCounter(20):setCounter(counter+1);
+    
+  }
+
+  function decrement() {
+    // if(counter <= 0)
+    // setCounter(counter - 1);
+    (counter<=0)? setCounter(0): setCounter(counter - 1);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>Counter Value : {counter}</h1>
+        <button onClick={increment}>Increment {counter}</button> <br /> <br />
+        <button onClick={decrement}>Decrement {counter}</button>
+        <p>The value of the counter is {counter}</p>
     </>
   )
 }
